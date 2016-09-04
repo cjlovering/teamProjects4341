bc68d106ac4027d880656390d56c36cf032a8046
cjlovering <cjlovering@130.215.14.76>
2016-09-03 21:20:18 -0400

    validate number types, and convert 'numbers' to numbers

--- assig1/parser.py
+++ assig1/parser.py
@@ -8,31 +8,39 @@ def parse(argv):
    inputfile = ''
    outputfile = ''
    try:
-      opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+     opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
    except getopt.GetoptError:
-      print('test.py -i <inputfile> -o <outputfile>')
-      sys.exit(2)
+     print('test.py -i <inputfile> -o <outputfile>')
+     sys.exit(2)
    for opt, arg in opts:
-      if opt == '-h':
-         print('test.py -i <inputfile> -o <outputfile>')
-         sys.exit()
-      elif opt in ("-i", "--ifile"):
-         inputfile = arg
-      elif opt in ("-o", "--ofile"):
-         outputfile = arg
+     if opt == '-h':
+       print('test.py -i <inputfile> -o <outputfile>')
+       sys.exit()
+     elif opt in ("-i", "--ifile"):
+       inputfile = arg
+     elif opt in ("-o", "--ofile"):
+       outputfile = arg
 
    testfile = open(inputfile, "r")
    alg = testfile.readline().strip(' \t\n\r')
-   startnum = testfile.readline().strip(' \t\n\r')
-   endnum = testfile.readline().strip(' \t\n\r')
-   time = testfile.readline().strip(' \t\n\r')
+   startnum = num(testfile.readline().strip(' \t\n\r'))
+   endnum = num(testfile.readline().strip(' \t\n\r'))
+   time = num(testfile.readline().strip(' \t\n\r'))
    tempOps = testfile.read().split()
    ops = []
    for index in range(0, len(tempOps), 2):
-     ops.append({tempOps[index], tempOps[index+1]})
+     ops.append((tempOps[index], num(tempOps[index+1])))
 
    problem = Problem( alg, startnum, endnum, time, ops )
    return problem
+
+# @param s - number to be converted
+# @return - appropriately converted number
+def num(s):
+  try:
+    return int(s)
+  except ValueError:
+    return float(s)
 
 if __name__ == "__main__":
    parse(sys.argv[1:])
--- assig1/problem.py
+++ assig1/problem.py
@@ -1,11 +1,24 @@
+from types import *
+import types
+
 #class to bundle problem parameters
 class Problem:
   def __init__(self, alg, startnum, targetnum, time, ops):
+    NumberTypes = (int, float)
+    assert isinstance(alg, str), "alg is not an string: %r" % alg
+    assert isinstance(startnum, NumberTypes), "startnum is not a integer: %r" % startnum
+    assert isinstance(targetnum, NumberTypes), "targetnum is not a integer: %r" % targetnum
+    assert isinstance(time, NumberTypes), "time is not a integer: %r" % time
+    for op in ops:
+      assert isinstance(op[0], str), "operator is not a string: %r" % op[0]
+      assert isinstance(op[1], NumberTypes), "operand is not a integer: %r" % op[1]
+
+
     self.alg = alg
     self.startnum = startnum
     self.targetnum = targetnum
     self.time = time
     self.ops = ops
 
-  def printProblem(self):
+  def printProblem(self): 
     print (self.alg, self.startnum, self.targetnum, self.time, self.ops)
