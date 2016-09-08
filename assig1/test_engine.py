f1825027328c93d05b1b7da8b87bf09862ab09f8
cjlovering <cjlovering@130.215.14.76>
2016-09-07 23:42:30 -0400

    protection vs .DS_STORE in test_engine

--- assig1/test_engine.py
+++ assig1/test_engine.py
@@ -2,14 +2,15 @@ from main import main
 from parser import parseFile
 from os import listdir
 from os.path import isfile, join
+import re
 
 testfiles = [f for f in listdir("./tests") if isfile(join("./tests", f))]
 
 solutions = []
 
 for file in testfiles:
-  solutions.append(main(parseFile("tests/" + file), False))
+  if re.match('test', file) is not None:
+    solutions.append(main(parseFile("tests/" + file), False))
 
 for fi in range(0, len(testfiles), 2):
   print(solutions[fi], solutions[fi+1])
-
