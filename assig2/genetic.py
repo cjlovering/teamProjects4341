77f336ac4f4ab9fccff79bc27b1bfcf879e2d447
cjlovering <cjlovering@130.215.14.76>
2016-09-16 17:23:00 -0400

    elitism outline

--- assig2/genetic.py
+++ assig2/genetic.py
@@ -22,6 +22,7 @@ def solve(problem):
   best = None
   target = problem.targetnum
   result = best
+  elitism = 1
   
   starting_op_count = 5;#round( math.log(problem.targetnum) / math.log(problem.startnum)) + 1
   population = []
@@ -42,8 +43,15 @@ def solve(problem):
       org.set_data(problem)
     if cut_off(population, start_time, problem.time, target):
       break;
+    
     new_population = []
     calculate_fitness(population, problem)
+
+    # elitism
+    population.sort(key=lambda x: x.fitness, reverse=True)
+
+    for i in range(elitism):
+      new_population.append(population[i])
 
     for i in range(len(population)):
       x = random_selection(population);
@@ -52,30 +60,35 @@ def solve(problem):
       if small_random_chance():
         child = mutate(child)
       new_population.append(child)
+    
+    # culling
+    #population.sort(key=lambda x: x.fitness, reverse=True) (remove the last n)
     population = new_population
+  
   return (best.data, organism_size, time.time()-start_time, len(population), generation)
 
 def calculate_fitness(population, problem):
   sum_costs = 0
-  charlie_s_magic_number = 2 / len(population)
   for org in population:
     org.set_data(problem)
     org.calculate_cost(problem)
     sum_costs += org.cost
   for org in population:
-    org.set_fitness(charlie_s_magic_number - org.cost / sum_costs)
+    org.set_fitness(1 - (org.cost / sum_costs))
 
 def random_selection(population):
-  percentile = random.uniform(0.0, 1.0) #ex 0.11
+  percentile = random.uniform(0.0, len(population) - 1) #this is the sum of all fitness values
   current_percent = 0
+  last_percent = 0
   selection = None
 
   for org in population:
     current_percent += org.fitness
-    if percentile <= current_percent:
+    if percentile <= current_percent and percentile > last_percent:
       selection = org
       break
-
+    else:
+      last_percent = current_percent
   return selection
 
 def cut_off(population, start_time, end_time, target):
